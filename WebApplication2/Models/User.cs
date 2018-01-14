5665bed9cb2faa7e0c234634a6c7a0267fecbb83
Sameera Desai <desaisameera@users.noreply.github.com>
2018-01-14 14:33:22 -0500

    Removed my account information

--- WebApplication2/Models/User.cs
+++ WebApplication2/Models/User.cs
@@ -7,10 +7,10 @@ namespace WebApplication2.Models
 {
     public class User
     {
-        public const string CONSUMER_KEY = "KX9AJkrb7kCTNlCo2S1Y05jjl";
-        public const string CONSUMER_SECRET = "yDRG4tSUcUQeNRqfHS7JMhoU3zX6X5MbumGYrP221YPLqNNSRe";
-        public const string ACCESS_TOKEN = "1623325351-9OnfR6xNA5mndiPy7FMFCZdVhRMyDzAy4T021me";
-        public const string ACCESS_SECRET = "h6QhQcVlVmTIKRIBpQNhwKZPLXkwEI7gHsHLvbBKkcRv4";
+        public const string CONSUMER_KEY = "YOUR_CONSUMER_KEY";
+        public const string CONSUMER_SECRET = "YOUR_CONSUMER_SECRET";
+        public const string ACCESS_TOKEN = "YOUR_ACCESS_TOKEN";
+        public const string ACCESS_SECRET = "YOUR_ACCESS_SECRET";
         public const string SIGNATURE_METHOD = "HMAC-SHA1";
         public const string OAUTH_VERSION = "1.0";
     }
